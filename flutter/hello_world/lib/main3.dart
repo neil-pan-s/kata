2e1cd4f5c3a4beb76cfcbc24a694892cddda8a00
Neil Pan <neil@Pax.local>
2019-01-14 17:44:22 +0800

    + flutter hello_world

---
+++ flutter/hello_world/lib/main3.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+class MyAppBar extends StatelessWidget {
+  MyAppBar({this.title});
+
+  // Fields in a Widget subclass are always marked "final".
+
+  final Widget title;
+
+  @override
+  Widget build(BuildContext context) {
+    // method.1: adapt to notch screen 
+    final double topPadding = MediaQuery.of(context).padding.top;
+    final double bottomPadding = MediaQuery.of(context).padding.bottom;
+
+    // method.2: use SafeArea Widget, need to set SafeArea's background, but how? 
+    // return new SafeArea(
+    //   child: new Container(
+    return new Container(
+        // method.1: adapt to notch screen 
+        height: 56.0 + topPadding, // in logical pixels
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        decoration: new BoxDecoration(color: Colors.blue[500]),
+        // Row is a horizontal, linear layout.
+        child: new Row(
+          // <Widget> is the type of items in the list.
+          children: <Widget>[
+            new IconButton(
+              icon: new Icon(Icons.menu),
+              tooltip: 'Navigation menu',
+              onPressed: null, // null disables the button
+            ),
+            // Expanded expands its child to fill the available space.
+            new Expanded(
+              child: title,
+            ),
+            new IconButton(
+              icon: new Icon(Icons.search),
+              tooltip: 'Search',
+              onPressed: null,
+            ),
+          ],
+        ),
+    );
+  }
+}
+
+class MyScaffold extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Material is a conceptual piece of paper on which the UI appears.
+    return new Material(
+      // Column is a vertical, linear layout.
+      child: new Column(
+        children: <Widget>[
+          new MyAppBar(
+            title: new Text(
+              'Example title',
+              style: Theme.of(context).primaryTextTheme.title,
+            ),
+          ),
+          new Expanded(
+            child: new Center(
+              child: new Text('Hello, world!'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(new MaterialApp(
+    title: 'My app', // used by the OS task switcher
+    home: new MyScaffold(),
+  ));
+}
